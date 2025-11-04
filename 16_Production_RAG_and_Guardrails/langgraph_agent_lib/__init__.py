943d585cf42be0d4d1e7f86256b6e465c2847b6f
sl2902 <laxmsun@hotmail.com>
2025-11-05 01:42:33 +0530

    update: added couple of modules

--- 16_Production_RAG_and_Guardrails/langgraph_agent_lib/__init__.py
+++ 16_Production_RAG_and_Guardrails/langgraph_agent_lib/__init__.py
@@ -4,6 +4,7 @@ A library for LangGraph agents with caching, monitoring, and agent integration.
 """
 
 from .agents import create_langgraph_agent
+from .helpfulness_agent import create_langgraph_helpfulness_agent, create_guarded_helpfulness_agent
 from .caching import CacheBackedEmbeddings, setup_llm_cache
 from .rag import ProductionRAGChain
 from .models import get_openai_model
@@ -11,6 +12,8 @@ from .models import get_openai_model
 __version__ = "0.1.0"
 __all__ = [
     "create_langgraph_agent",
+    "create_langgraph_helpfulness_agent",
+    "create_guarded_helpfulness_agent",
     "CacheBackedEmbeddings",
     "setup_llm_cache",
     "ProductionRAGChain",
